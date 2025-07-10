cc3c1c66514d147e3e24fc8e2de048733b07d3d8
harryzhaozy <112836335+harryzhaozy@users.noreply.github.com>
2025-07-11 06:56:58 +0800

    Update LiteApple.java

--- app/src/main/java/com/github/catvod/spider/LiteApple.java
+++ app/src/main/java/com/github/catvod/spider/LiteApple.java
@@ -8,12 +8,10 @@ import android.util.Base64;
 
 import com.github.catvod.crawler.Spider;
 import com.github.catvod.crawler.SpiderDebug;
-import com.github.catvod.utils.Misc;
-import com.github.catvod.net.OKCallBack;
-import com.github.catvod.net.OkHttpUtil;
+
+import com.github.catvod.net.OkHttp;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
@@ -26,86 +24,27 @@ import java.util.List;
 import java.util.Random;
 import java.util.Set;
 
-import okhttp3.Call;
 
 public class LiteApple extends Spider {
     private static final String siteUrl = "http://ht.grelighting.cn/api.php";
 
     private HashMap<String, String> getHeaders(String url, String data) {
         HashMap<String, String> headers = new HashMap<>();
-        if (data != null) {
-            String token = "";
-            try {
-                token = Base64.encodeToString(b(fakeDevice.getBytes("UTF-8"), tokenKey == null ? "XPINGGUO" : tokenKey), 2);
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
-            int currentTimeMillis = (int) (System.currentTimeMillis() / ((long) 1000));
-            String hash = md5(fakeDevice + data + currentTimeMillis).substring(8, 12);
-            headers.put("token", token);
-            headers.put("hash", hash);
-            headers.put("timestamp", currentTimeMillis + "");
-            if (tokenKey == null) {
-                headers.put("version", "ANDROID cn.grelighting.xpg1.1.5");
-            }
-        }
-        headers.put("User-Agent", "okhttp/4.9.1");
+        headers.put("User-Agent", "http/4.9.1 Lvaf/58.12.100");
         return headers;
     }
 
-    private String fakeDevice = null;
-    private String tokenKey = null;
-
     @Override
     public void init(Context context, String extend) {
-        try {
-            super.init(context, extend);
-        } catch (Exception e) {
-            e.printStackTrace(); // 或 Log.e("TAG", "init失败", e);
-        }
-
-        SharedPreferences sharedPreferences = context.getSharedPreferences("spider_LiteApple", Context.MODE_PRIVATE);
-        try {
-            fakeDevice = sharedPreferences.getString("didd", null);
-        } catch (Throwable th) {
-            th.printStackTrace();
-        } finally {
-            if (fakeDevice == null) {
-                fakeDevice = fakeDid();
-                sharedPreferences.edit().putString("didd", fakeDevice).apply();
-            }
-        }
-    }
-
-
-    void getTokenKey() {
-        if (tokenKey != null)
-            return;
-        String url = siteUrl + "/v2.user/tokenlogin";
-        OkHttpUtil.post(OkHttpUtil.defaultClient(), url, null, getHeaders(url, "ANDROID cn.grelighting.xpg1.1.5"), new OKCallBack.OKCallBackString() {
-            @Override
-            protected void onFailure(Call call, Exception e) {
-
-            }
-
-            @Override
-            protected void onResponse(String response) {
-                try {
-                    tokenKey = new JSONObject(response).getJSONObject("data").getString("user_title");
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+        super.init(context, extend);
     }
 
     @Override
     public String homeContent(boolean filter) {
         try {
-            getTokenKey();
             String url = siteUrl + "/v2.vod/androidtypes";
-            String content = OkHttpUtil.string(url, getHeaders(url, null));
-            JSONObject jsonObject = new JSONObject(decryptResponse(content));
+            String content = Http.string(url, getHeaders(url, null));
+            JSONObject jsonObject = new JSONObject(content);
             JSONArray jsonArray = jsonObject.getJSONArray("data");
 
             JSONObject filterConfig = new JSONObject();
@@ -220,8 +159,8 @@ public class LiteApple extends Spider {
                     break;
                 try {
                     String url = siteUrl + "/v2.main/androidhome";
-                    String content = OkHttpUtil.string(url, getHeaders(url, null));
-                    JSONObject jsonObject = new JSONObject(decryptResponse(content));
+                    String content = OkHttp.string(url, getHeaders(url, null));
+                    JSONObject jsonObject = new JSONObject(content);
                     JSONArray jsonArray = jsonObject.getJSONObject("data").getJSONArray("list");
                     for (int i = 0; i < jsonArray.length(); i++) {
                         JSONArray jsonArraySub = jsonArray.getJSONObject(i).getJSONArray("list");
@@ -251,7 +190,6 @@ public class LiteApple extends Spider {
     @Override
     public String categoryContent(String tid, String pg, boolean filter, HashMap<String, String> extend) {
         try {
-            getTokenKey();
             String area = extend.containsKey("area") ? extend.get("area") : "";
             String year = extend.containsKey("year") ? extend.get("year") : "";
             String url = siteUrl + "/v2.vod/androidfilter?page=" + pg + "&type=" + tid;
@@ -262,8 +200,8 @@ public class LiteApple extends Spider {
                     continue;
                 url += "&" + key + "=" + URLEncoder.encode(val);
             }
-            String content = OkHttpUtil.string(url, getHeaders(url, area + year));
-            JSONObject dataObject = new JSONObject(decryptResponse(content));
+            String content = OkHttp.string(url, getHeaders(url, area + year));
+            JSONObject dataObject = new JSONObject(content);
             JSONArray jsonArray = dataObject.getJSONArray("data");
             JSONArray videos = new JSONArray();
             for (int i = 0; i < jsonArray.length(); i++) {
@@ -295,8 +233,8 @@ public class LiteApple extends Spider {
     public String detailContent(List<String> ids) {
         try {
             String url = siteUrl + "/v2.vod/androiddetail?vod_id=" + ids.get(0);
-            String content = OkHttpUtil.string(url, getHeaders(url, ids.get(0)));
-            JSONObject dataObject = new JSONObject(decryptResponse(content));
+            String content = OkHttp.string(url, getHeaders(url, ids.get(0)));
+            JSONObject dataObject = new JSONObject(content);
             JSONObject vObj = dataObject.getJSONObject("data");
             JSONObject result = new JSONObject();
             JSONArray list = new JSONArray();
@@ -333,15 +271,12 @@ public class LiteApple extends Spider {
     @Override
     public String playerContent(String flag, String id, List<String> vipFlags) {
         try {
-            if (Misc.isVideoFormat(id)) {
-                JSONObject result = new JSONObject();
-                result.put("parse", 0);
-                result.put("header", "");
-                result.put("playUrl", "");
-                result.put("url", id);
-                return result.toString();
-            }
-            return "";
+            JSONObject result = new JSONObject();
+            result.put("parse", 0);
+            result.put("header", new JSONObject(getHeaders(id, null)).toString());
+            result.put("playUrl", "");
+            result.put("url", id);
+            return result.toString();
         } catch (Exception e) {
             SpiderDebug.log(e);
         }
@@ -351,10 +286,9 @@ public class LiteApple extends Spider {
     @Override
     public String searchContent(String key, boolean quick) {
         try {
-            getTokenKey();
             String url = siteUrl + "/v2.vod/androidsearch?page=1&wd=" + URLEncoder.encode(key);
-            String content = OkHttpUtil.string(url, getHeaders(url, key));
-            JSONObject dataObject = new JSONObject(decryptResponse(content));
+            String content = OkHttp.string(url, getHeaders(url, key));
+            JSONObject dataObject = new JSONObject(content);
             JSONArray jsonArray = dataObject.getJSONArray("data");
             JSONArray videos = new JSONArray();
             for (int i = 0; i < jsonArray.length(); i++) {
@@ -377,97 +311,4 @@ public class LiteApple extends Spider {
         }
         return "";
     }
-
-    protected String decryptResponse(String src) {
-        return src;
-    }
-
-    byte[] a(String str) {
-        byte[] bytes = str.getBytes();
-        byte[] bArr = new byte[333];
-        for (int i9 = 0; i9 < 333; i9++) {
-            bArr[i9] = (byte) i9;
-        }
-        if (bytes.length == 0) {
-            return null;
-        }
-        int i10 = 0;
-        int i11 = 0;
-        for (int i12 = 0; i12 < 333; i12++) {
-            i11 = (((bytes[i10] & 255) + (bArr[i12] & 255)) + i11) % 333;
-            byte b = bArr[i12];
-            bArr[i12] = bArr[i11];
-            bArr[i11] = b;
-            i10 = (i10 + 1) % bytes.length;
-        }
-        return bArr;
-    }
-
-    byte[] b(byte[] bArr, String str) {
-        byte[] a = a(str);
-        byte[] bArr2 = new byte[bArr.length];
-        int i9 = 0;
-        int i10 = 0;
-        for (int i11 = 0; i11 < bArr.length; i11++) {
-            i9 = (i9 + 1) % 333;
-            i10 = ((a[i9] & 255) + i10) % 333;
-            byte b = a[i9];
-            a[i9] = a[i10];
-            a[i10] = b;
-            bArr2[i11] = (byte) (a[((a[i9] & 255) + (a[i10] & 255)) % 333] ^ bArr[i11]);
-        }
-        return bArr2;
-    }
-
-    String randomString(int length) {
-        String str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456";
-        Random random = new Random();
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < length; i++) {
-            int number = random.nextInt(str.length());
-            sb.append(str.charAt(number));
-        }
-        return sb.toString();
-    }
-
-    String randomMACAddress() {
-        Random rand = new Random();
-        byte[] macAddr = new byte[6];
-        rand.nextBytes(macAddr);
-        macAddr[0] = (byte) (macAddr[0] & (byte) 254);  //zeroing last 2 bytes to make it unicast and locally adminstrated
-        StringBuilder sb = new StringBuilder(18);
-        for (byte b : macAddr) {
-            sb.append(String.format("%02X", b));
-        }
-        return sb.toString();
-    }
-
-    String fakeDid() {
-        String i = "";
-        String f = "";
-        String d = "N/A";
-        try {
-            d = Build.class.getField("SERIAL").get((Object) null).toString();
-        } catch (Exception unused) {
-        }
-        String e = "";
-        try {
-            e = (String) Class.forName("android.os.SystemProperties").getDeclaredMethod("get", new Class[]{String.class}).invoke((Object) null, new Object[]{"ro.build.fingerprint"});
-        } catch (Exception unused) {
-            return "";
-        }
-        return (((((((((("" + i) + "||") + f) + "||") + randomMACAddress()) + "||") + randomString(16)) + "||") + d) + "||") + e;
-    }
-
-    String md5(String str) {
-        try {
-            MessageDigest instance = MessageDigest.getInstance("MD5");
-            instance.update(str.getBytes());
-            return new BigInteger(1, instance.digest()).toString(16);
-        } catch (Exception e9) {
-            e9.printStackTrace();
-            return str;
-        }
-    }
-
 }
