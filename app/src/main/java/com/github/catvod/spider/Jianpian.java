4656c8fd9df61972c1c54daa858935d7cccc11c4
harryzhaozy <112836335+harryzhaozy@users.noreply.github.com>
2026-08-12 16:19:18 +0800

    Update Jianpian.java

--- app/src/main/java/com/github/catvod/spider/Jianpian.java
+++ app/src/main/java/com/github/catvod/spider/Jianpian.java
@@ -25,7 +25,7 @@ import java.util.Map;
  */
 public class Jianpian extends Spider {
 
-    private final String siteUrl = "https://ev5356.970xw.com";
+    private final String siteUrl = "https://fan123.shmjnd.com";
     private String extend;
 
     private Map<String, String> getHeader() {
