966ad9ad74ddf70b21b4ff0ea08bebca36f66d32
harryzhaozy <112836335+harryzhaozy@users.noreply.github.com>
2026-09-09 08:30:41 +0800

    Update Bili.java

--- app/src/main/java/com/github/catvod/spider/Bili.java
+++ app/src/main/java/com/github/catvod/spider/Bili.java
@@ -60,7 +60,7 @@ import java.util.concurrent.TimeUnit;
  */
 public class Bili extends Spider {
 
-    private static final String COOKIE = "buvid3=7238A82F-D821-D23A-FAA5-ADC19C9796B458050infoc;bsource=search_baidu;_uuid=628F6D84-DF8D-ED5A-E4BF-66219979424B58395infoc;  buvid_fp=f7761c3c9bde36415f0299493c60b971;bp_t_offset_55423440=1203047221809905664;b_lsid=C1568F05_19E3B3AD0DE";
+    private static final String COOKIE = "buvid3=8B57D3BA-607A-1E85-018A-E8C430023CED42659infoc; b_lsid=BEB8EE7F_18742FF8C2E; bsource=search_baidu; _uuid=DE810E367-B52C-AF6E-A612-EDF4C31567F358591infoc; b_nut=100; buvid_fp=711a632b5c876fa8bbcf668c1efba551; SESSDATA=7624af93%2C1696008331%2C862c8%2A42; bili_jct=141a474ef3ce8cf2fedf384e68f6625d; DedeUserID=3493271303096985; DedeUserID__ckMd5=212a836c164605b7; sid=5h4ruv6o; buvid4=978E9208-13DA-F87A-3DC0-0B8EDF46E80434329-123040301-dWliG5BMrUb70r3g583u7w%3D%3D";
     private static String cookie;
 
     private JsonObject extend;
